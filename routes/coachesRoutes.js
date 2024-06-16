794f83680bb73fbfadb5695afc175bdf445dacc5
z5om76 <95319049+z5om76@users.noreply.github.com>
2024-06-16 17:12:57 +0100

    Update coachesRoutes.js

--- routes/coachesRoutes.js
+++ routes/coachesRoutes.js
@@ -2,9 +2,33 @@ const express = require('express')
 const router = express.Router()
 const employesController = require('../Controllers/employesController')
 
-router.route('/')
+router.route('/therapist')
     
-      .get(employesController.getCoaches)
+      .get(employesController.getTherapist)
+    
+    
+
+module.exports = router
+
+router.route('/SCC')
+    
+      .get(employesController.getSCC)
+    
+    
+
+module.exports = router
+
+router.route('/SSC')
+    
+      .get(employesController.getSSC)
+    
+    
+
+module.exports = router
+
+router.route('/FCSC')
+    
+      .get(employesController.getFCSC)
     
     
 
